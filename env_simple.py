99b65fa7b7834b2a23ab46b018bb361ffa803b57
gabrielibagon <gabriel.ibagon@gmail.com>
2016-12-16 16:36:13 -0500

    draft of flanker environment

--- env_simple.py
+++ env_simple.py
@@ -4,7 +4,7 @@ import time
 
 #ENVIRONMENTAL VARIABLES FOR GAME
 #colors
-BLACK = (0,0,0)     # background
+BLACK = (0,0,0)     	# background
 WHITE = (255,255,255) # agent
 GREEN = (0, 255, 0)   # goal
 RED = (255, 0, 0)     # mistake
@@ -13,21 +13,9 @@ RED = (255, 0, 0)     # mistake
 width = 1000
 height = 1000
 screen = pygame.display.set_mode([width, height])
-
+pygame.display.set_caption("Simple Error Task")
 # recentangle side length:
 length = 80
-
-pygame.display.set_caption("Simple Error Task")
-
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# Starting position of the rectangle
-rect_x = width / 2
-rect_y = height / 2
 
 # Positions of goal [x1,y1, x2,y2]
 TOP = [rect_x - length/2, 0, length,length]
@@ -36,58 +24,72 @@ RIGHT = [width - length, rect_y - length/2, length, length]
 LEFT = [0, rect_y - length/2, length, length]
 positions = [TOP, BOTTOM, RIGHT, LEFT]
 
-#initial position of the agent
+# Starting position of the rectangle
+rect_x = width / 2
+rect_y = height / 2
 initial_pos = [rect_x - length/2, rect_y - length/2, length, length]
 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
 
-# -------- Main Program Loop -----------
-while not done:
-	# --- Event Processing
-	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-					done = True
+class Environment():
+	def __init__(self):
 
-	#temporary: randomly pick the goal
-	goal = positions[0]
-	action = positions[random.randint(0,3)]
-	current_pos = initial_pos[:]
+		# Loop until the user clicks the close button.
+		self.done = False
+		 
+	def run(self):
+		# -------- Main Program Loop -----------
+		while not done:
+			# --- Event Processing
+			for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+							done = True
 
-	screen.fill(BLACK)	
-	pygame.draw.rect(screen, WHITE, current_pos) 	#agent
-	pygame.draw.rect(screen, GREEN, goal)				#goal
-	pygame.display.flip()
-	time.sleep(1)
-	while current_pos not in positions:
-		# Set the screen background
-		screen.fill(BLACK)
+			#temporary: randomly pick the goal
+			goal = positions[0]
+			action = positions[random.randint(0,3)]
+			current_pos = initial_pos[:]
 
-		# Draw the rectangles
-		pygame.draw.rect(screen, WHITE, current_pos) 	#agent
-		pygame.draw.rect(screen, GREEN, goal)				#goal
+			screen.fill(BLACK)	
+			pygame.draw.rect(screen, WHITE, current_pos) 	#agent
+			pygame.draw.rect(screen, GREEN, goal)				#goal
+			pygame.display.flip()
+			time.sleep(1)
+			while current_pos not in positions:
+				# Set the screen background
+				screen.fill(BLACK)
 
-		# --- Wrap-up
-		# Limit to 60 frames per second
-		clock.tick(60)
+				# Draw the rectangles
+				pygame.draw.rect(screen, WHITE, current_pos) 	#agent
+				pygame.draw.rect(screen, GREEN, goal)				#goal
 
-		# next position:
-		for i, (a,b) in enumerate(zip(action,current_pos)):
-			if a-b > 0:
-				current_pos[i] += 10
-			elif a-b < 0:
-				current_pos[i] -= 10
+				# --- Wrap-up
+				# Limit to 60 frames per second
+				clock.tick(60)
 
-		# update the screen
-		pygame.display.flip()
+				# next position:
+				for i, (a,b) in enumerate(zip(action,current_pos)):
+					if a-b > 0:
+						current_pos[i] += 10
+					elif a-b < 0:
+						current_pos[i] -= 10
+				pygame.display.flip()
 
-	# draw the last frame of the trial
-	screen.fill(BLACK)	
-	pygame.draw.rect(screen, WHITE, current_pos) 	#agent
-	pygame.draw.rect(screen, GREEN, goal)				#goal
-	pygame.display.flip()
+			# draw the last frame of the trial
+			screen.fill(BLACK)	
+			pygame.draw.rect(screen, WHITE, current_pos) 	#agent
+			pygame.draw.rect(screen, GREEN, goal)				#goal
+			pygame.display.flip()
+			time.sleep(.5)
+
+ 		self.exit()
+
+ 	def exit(self):
+		# Close everything down
+		pygame.display.quit()
+		pygame.quit()
 
 
-	time.sleep(.5)
- 
-# Close everything down
-pygame.display.quit()
-pygame.quit()
+env = Environment()
+env.run()
---
+++ flanker.py
@@ -0,0 +1,89 @@
+import pygame
+from random import randint
+import os
+import time
+from pylsl import StreamInfo, StreamOutlet
+
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+WIDTH = 1000
+HEIGHT = 1000
+
+
+
+class FlankerTask():
+	def __init__(self):
+		# screen
+		self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
+		pygame.display.set_caption("Flanker Task")
+		clock = pygame.time.Clock()
+
+		# LSL setup
+		info = StreamInfo('MarkerStream','Markers',1, 0,'string','markerstream')
+		self.outlet = StreamOutlet(info)
+		self.markers = ['Correct','Incorrect']
+
+		# start task
+		self.start_task()
+
+
+	def start_task(self):
+		options = [pygame.K_LEFT, pygame.K_RIGHT]
+		while True:
+			pygame.event.set_blocked(pygame.KEYDOWN)
+			self.screen.fill(BLACK)	
+			pygame.display.flip()
+			time.sleep(1)
+			direction = self.draw_arrows()
+			pygame.display.flip()
+			timeout = time.time() + 2
+			pygame.event.get()
+			pygame.event.set_allowed(pygame.KEYDOWN)
+			while time.time() < timeout:
+				pygame.event.clear()
+				event = pygame.event.poll()
+				# print(event)
+				if event.type == pygame.KEYDOWN and event.key in options:
+					print("TEST")
+					if event.key == pygame.K_LEFT:
+						user_input = 1
+					elif event.key == pygame.K_RIGHT:
+						user_input = 0
+					if user_input == direction:
+						print(self.markers[0])
+						self.outlet.push_sample([self.markers[0]])
+					elif user_input == (direction ^ 1):
+						print(self.markers[1])
+						self.outlet.push_sample([self.markers[1]])
+					pygame.event.set_blocked(pygame.KEYDOWN)
+					break;	
+
+	def draw_arrows(self):
+		stim_type = randint(0,2) 		# arrow group incongruent, congruent, or neutral
+		direction = randint(0,1)		# choose direction (left or right)
+		segment = WIDTH/5
+		flanker_coord = [(50,HEIGHT/2 - 50), (250, HEIGHT/2 - 50), (650, HEIGHT/2 -50), (850, HEIGHT/2 - 50)]
+		right_arrow = pygame.transform.scale(pygame.image.load('arrow.png'), (150,100))
+		left_arrow = pygame.transform.flip(right_arrow,1,0)
+		arrows = [right_arrow, left_arrow]
+		# DRAW
+		## central arrow:
+		self.screen.blit(arrows[direction],(450, HEIGHT/2 - 50))
+		if stim_type == 0:
+			flank_dir = (direction ^ 1)
+		elif stim_type == 1:
+			flank_dir = direction
+		elif stim_type == 2:
+			return direction
+
+		# flanker arrows
+		for coord in flanker_coord:
+			self.screen.blit(arrows[flank_dir],coord)
+		return direction
+
+
+if __name__ == '__main__':
+	ft = FlankerTask()
+	
