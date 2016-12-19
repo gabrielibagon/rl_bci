cd10fc6ca5d8ebeb3217e1b7330eb4feaeac90cf
gabrielibagon <gabriel.ibagon@gmail.com>
2016-12-19 13:16:12 -0500

    combined flanker.py and flanker_calibrate.py

--- flanker.py
+++ flanker.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 import os
+import sys
 import time
 from pylsl import StreamInfo, StreamOutlet
 
@@ -11,35 +12,33 @@ WHITE = (255,255,255)
 WIDTH = 1000
 HEIGHT = 1000
 
-
-
 class FlankerTask():
-	def __init__(self):
+	def __init__(self,calib):
 		# screen
 		self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
 		pygame.display.set_caption("Flanker Task")
-		clock = pygame.time.Clock()
 
 		# LSL setup
-		info_button = StreamInfo('button_presses','Markers',1, 0,'string','markerstream')
-		info_correct = StreamInfo('correctness','Markers',1, 0,'string','markerstream')
+		info_button = StreamInfo('button_presses','Markers',1, 0,'string','markerstream1')
+		info_correct = StreamInfo('correctness','Markers',1, 0,'string','markerstream2')
 		self.outlet_button = StreamOutlet(info_button)
 		self.outlet_correct = StreamOutlet(info_correct)
 		self.markers = ['Correct','Incorrect']
 		# start task
-		self.start_task()
+		if calib:
+			for i in range(4):
+				time.sleep(15)
+				self.start_task(True)
+		else:
+			self.start_task(False)
 
-
-
-	def start_task(self):
+	def start_task(self,calib):
 		options = [pygame.K_LEFT, pygame.K_RIGHT]
-		clock = pygame.time.Clock()
 		count=0
 		while count<20:
 			self.screen.fill(BLACK)	
 			pygame.display.flip()
 			time.sleep(1)
-			
 			direction = self.draw_arrows()
 			pygame.display.flip()
 			pygame.event.set_allowed(None)
@@ -47,7 +46,8 @@ class FlankerTask():
 			pygame.event.clear(pygame.KEYDOWN)
 			event = pygame.event.wait()
 			pygame.event.set_blocked(pygame.KEYDOWN)
-			self.outlet_button.push_sample(['Button'])
+			if not calib:
+				self.outlet_button.push_sample(['Button'])
 			if event.type == pygame.KEYDOWN and event.key in options:
 				if event.key == pygame.K_LEFT:
 					user_input = 1
@@ -58,14 +58,15 @@ class FlankerTask():
 					self.outlet_correct.push_sample([self.markers[0]])
 				elif user_input == (direction ^ 1):
 					print(self.markers[1])
-					self.outlet_correct.push_sample([self.markers[0]])
+					self.outlet_correct.push_sample([self.markers[1]])
 				pygame.event.set_blocked(pygame.KEYDOWN)
 				count+=1
 			pygame.event.pump()
+		self.outlet_button.push_sample('End')
 
 
 	def draw_arrows(self):
-		stim_type = randint(0,2) 		# arrow group incongruent, congruent, or neutral
+		stim_type = randint(0,1) 		# arrow group incongruent, congruent, or neutral
 		direction = randint(0,1)		# choose direction (left or right)
 		segment = WIDTH/5
 		flanker_coord = [(50,HEIGHT/2 - 50), (250, HEIGHT/2 - 50), (650, HEIGHT/2 -50), (850, HEIGHT/2 - 50)]
@@ -79,8 +80,6 @@ class FlankerTask():
 			flank_dir = (direction ^ 1)
 		elif stim_type == 1:
 			flank_dir = direction
-		elif stim_type == 2:
-			return direction
 
 		# flanker arrows
 		for coord in flanker_coord:
@@ -89,5 +88,8 @@ class FlankerTask():
 
 
 if __name__ == '__main__':
-	ft = FlankerTask()
+	calib = False
+	if len(sys.argv) > 1 and sys.argv[1] == '--calib':
+		calib = True
+	ft = FlankerTask(calib)
 	
